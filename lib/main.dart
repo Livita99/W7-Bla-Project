13266c2f9e9b86f70022443fab1121a5d0a14096
Livita <150226387+Livita99@users.noreply.github.com>
2025-03-28 01:01:30 +0700

    BLA-204  Implement the Local Storage repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:week_3_blabla_project/data/repository/local/local_ride_preferences_repository.dart';
 import 'package:week_3_blabla_project/ui/providers/rides_preferences_provider.dart';
 import 'data/repository/mock/mock_locations_repository.dart';
 import 'data/repository/mock/mock_rides_repository.dart';
 import 'service/locations_service.dart';
 import 'service/rides_service.dart';
 
-import 'data/repository/mock/mock_ride_preferences_repository.dart';
+// import 'data/repository/mock/mock_ride_preferences_repository.dart';
 import 'ui/screens/ride_pref/ride_pref_screen.dart';
 import 'ui/theme/theme.dart';
 
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(
           create: (_) => RidesPreferencesProvider(
-            repository: MockRidePreferencesRepository(),
+            repository: LocalRidePreferencesRepository(),
           ),
         ),
       ],
